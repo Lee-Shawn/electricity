1612474c67d20eb347e6124db9bfebba343e9829
ximingze <425984393@qq.com>
2021-06-30 23:42:46 +0800

    添加网关模块

---
+++ electricity-gateway/src/main/java/com/laughing/electricity/gateway/ElectricityGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.laughing.electricity.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ElectricityGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityGatewayApplication.class, args);
+	}
+
+}
---
+++ electricity-gateway/src/test/java/com/laughing/electricity/gateway/ElectricityGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laughing.electricity.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
